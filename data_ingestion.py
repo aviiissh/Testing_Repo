f615a96123a2647f605e99fdfcfae4e3a44f5691
aviiissh <avish2314@gmail.com>
2026-01-08 09:36:13 +0530

    some changes in barnch

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,7 +1,16 @@
 def ingest_csv(df):
-  some logic here
+  #some logic here
+  return df
 
 def ingest_json(df2):
-  some logic here
+  #some logic here
+  return df2
+def ingest_data(file_path, file_type):
+    if file_type == 'csv':
+        return ingest_csv(file_path)
+    elif file_type == 'json':
+        return ingest_json(file_path)
+    else:
+        raise ValueError("Unsupported file type: {}".format(file_type))
 
   
